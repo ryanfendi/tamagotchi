e9fb22132ff5625a305e7ef30b9a9648201a2a06
ryanfendi <102475046+ryanfendi@users.noreply.github.com>
2025-08-20 20:10:26 +0700

    firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,12 @@
+<!-- Muat SDK Firebase dari CDN di setiap halaman SEBELUM online.js -->
+<script type="module">
+  // GANTI isian di bawah dengan config milikmu dari Firebase console
+  export const firebaseConfig = {
+    apiKey: "AIzaSyAN0GNZdIx_xPHT7MZKWJweLtNhies6QxY",
+    authDomain: "tamagotchi-2be0c.firebaseapp.com",
+    projectId: "tamagotchi-2be0c",
+    storageBucket: "tamagotchi-2be0c.firebasestorage.app",
+    messagingSenderId: "202052432703",
+    appId: "1:202052432703:web:cb76b50e42149a2c92998a"
+  };
+</script>
